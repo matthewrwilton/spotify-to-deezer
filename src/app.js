10f55b5e77596fc204ba923503b267fbb8138cc0
matthewrwilton <matthew.wilton@onemodel.co>
2014-12-16 22:32:47 +1000

    Use Spotify API to display playlists

---
+++ src/app.js
@@ -0,0 +1,124 @@
+
+var util = require('util');
+var express = require('express');
+var request = require('request');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+var apiSecrets = require('./lib/api-secrets.js');
+
+var stateKey = 'spotify_auth_state';
+var redirect_uri = 'http://localhost:8888/callback';
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function(length) {
+
+	var text = '';
+	var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+	for (var i = 0; i < length; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+};
+
+var isBadResponse = function(error, responseStatusCode) {
+
+	if (error || responseStatusCode != 200) {
+		console.error('error: %j' + error);
+		console.error('response.statusCode: %d' + response.statusCode);
+		return true;
+	}
+	
+	return false;
+};
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'))
+	.use(cookieParser());
+
+app.get('/login', function(req, res) {
+
+	var state = generateRandomString(16);
+	res.cookie(stateKey, state);
+
+	var scope = 'playlist-read-private';
+	res.redirect('https://accounts.spotify.com/authorize?' +
+		querystring.stringify({
+			response_type: 'code',
+			client_id: apiSecrets.spotifyClientId,
+			scope: scope,
+			redirect_uri: redirect_uri,
+			state: state
+	}));
+});
+
+app.get('/callback', function(req, res) {
+
+	var code = req.query.code || null;
+	var state = req.query.state || null;
+	var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+	if (state === null || state !== storedState) {
+		res.redirect('/#' +
+			querystring.stringify({
+				error: 'state_mismatch'
+		}));
+	}
+	
+	res.clearCookie(stateKey);
+	var authOptions = {
+		url: 'https://accounts.spotify.com/api/token',
+		form: {
+			code: code,
+			redirect_uri: redirect_uri,
+			grant_type: 'authorization_code'
+		},
+		headers: {
+			'Authorization': 'Basic ' + (new Buffer(apiSecrets.spotifyClientId + ':' + apiSecrets.spotifyClientSecret).toString('base64'))
+		},
+		json: true
+	};
+
+	request.post(authOptions, function(error, response, body) {
+
+		if (isBadResponse(error, response.statusCode)) {
+			res.redirect('/#' +
+				querystring.stringify({
+					error: 'invalid_token'
+			}));
+		}
+
+		var access_token = body.access_token,
+			refresh_token = body.refresh_token;
+
+		var options = {
+			url: 'https://api.spotify.com/v1/me',
+			headers: { 'Authorization': 'Bearer ' + access_token },
+			json: true
+		};
+        request.get(options, function(error, response, body) {
+						
+			if (isBadResponse(error, response.statusCode)) {
+				res.redirect('/#' +
+					querystring.stringify({
+						error: 'invalid_user_details'
+				}));
+			}
+			
+			res.redirect('/#' +
+				querystring.stringify({
+					access_token: access_token,
+					refresh_token: refresh_token,
+					user_id: response.body.id
+			}));
+        });
+	});
+});
+
+console.log('Listening on 8888');
+app.listen(8888);
---
+++ src/lib/api-secrets.js
@@ -0,0 +1,6 @@
+
+var spotifyClientId = '03ffe0cac0a0401aa6673c3cf6d02ced';
+var spotifyClientSecret = 'a57c43efb9644574a96d6623fb8bfbc2';
+
+exports.spotifyClientId = spotifyClientId;
+exports.spotifyClientSecret = spotifyClientSecret;
