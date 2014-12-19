e4d562be9242cd721f02e3ab9295b1e511e03b92
matthewrwilton <matthew.wilton@onemodel.co>
2014-12-20 09:56:39 +1000

    Add Deezer variables into api-secrets

--- src/lib/api-secrets.js
+++ src/lib/api-secrets.js
@@ -2,5 +2,10 @@
 var spotifyClientId = '03ffe0cac0a0401aa6673c3cf6d02ced';
 var spotifyClientSecret = 'a57c43efb9644574a96d6623fb8bfbc2';
 
+var deezerAppId = '';
+var deezerSecretKey = '';
+
 exports.spotifyClientId = spotifyClientId;
 exports.spotifyClientSecret = spotifyClientSecret;
+exports.deezerAppId = deezerAppId;
+exports.deezerSecretKey = deezerSecretKey;
