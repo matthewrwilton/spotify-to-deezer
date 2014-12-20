6629df2a61b4dd34401121af611be21f6bf94a30
matthewrwilton <matthew.wilton@onemodel.co>
2014-12-21 07:22:00 +1000

    Login to Deezer

--- src/app.js
+++ src/app.js
@@ -6,8 +6,11 @@ var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 var apiSecrets = require('./lib/api-secrets.js');
 
-var stateKey = 'spotify_auth_state';
-var redirect_uri = 'http://localhost:8888/callback';
+var spotifyStateKey = 'spotify_auth_state';
+var spotifyRedirectUri = 'http://localhost:8888/spotifyCallback';
+
+var deezerStateKey = 'deezer_auth_state';
+var deezerCallbackUri = 'http://localhost:8888/deezerCallback';
 
 /**
  * Generates a random string containing numbers and letters
@@ -44,24 +47,23 @@ app.use(express.static(__dirname + '/public'))
 app.get('/spotifyLogin', function(req, res) {
 
 	var state = generateRandomString(16);
-	res.cookie(stateKey, state);
+	res.cookie(spotifyStateKey, state);
 
-	var scope = 'playlist-read-private';
 	res.redirect('https://accounts.spotify.com/authorize?' +
 		querystring.stringify({
 			response_type: 'code',
 			client_id: apiSecrets.spotifyClientId,
-			scope: scope,
-			redirect_uri: redirect_uri,
+			scope: 'playlist-read-private',
+			redirect_uri: spotifyRedirectUri,
 			state: state
 	}));
 });
 
-app.get('/callback', function(req, res) {
+app.get('/spotifyCallback', function(req, res) {
 
 	var code = req.query.code || null;
 	var state = req.query.state || null;
-	var storedState = req.cookies ? req.cookies[stateKey] : null;
+	var storedState = req.cookies ? req.cookies[spotifyStateKey] : null;
 
 	if (state === null || state !== storedState) {
 		res.redirect('/#' +
@@ -70,12 +72,12 @@ app.get('/callback', function(req, res) {
 		}));
 	}
 	
-	res.clearCookie(stateKey);
+	res.clearCookie(spotifyStateKey);
 	var authOptions = {
 		url: 'https://accounts.spotify.com/api/token',
 		form: {
 			code: code,
-			redirect_uri: redirect_uri,
+			redirect_uri: spotifyRedirectUri,
 			grant_type: 'authorization_code'
 		},
 		headers: {
@@ -115,6 +117,86 @@ app.get('/callback', function(req, res) {
 					spotify_access_token: access_token,
 					spotify_refresh_token: refresh_token,
 					spotify_user_id: response.body.id
+			}));
+        });
+	});
+});
+
+app.get('/deezerLogin', function(req, res) {
+
+	var deezerState = generateRandomString(16);
+	res.cookie(deezerStateKey, deezerState);
+	
+	res.redirect('https://connect.deezer.com/oauth/auth.php?' +
+		querystring.stringify({
+			app_id: apiSecrets.deezerAppId,
+			redirect_uri: deezerCallbackUri,
+			perms: 'basic_access,manage_library',
+			state: deezerState
+	}));
+});
+
+app.get('/deezerCallback', function(req, res) {
+
+	var code = req.query.code || null,
+		state = req.query.state || null,
+		storedState = req.cookies ? req.cookies[deezerStateKey] : null,
+		errorReason = req.query.error_reason || null;
+		
+	if (state === null || state !== storedState) {
+		res.redirect('/#' +
+			querystring.stringify({
+				error: 'state_mismatch'
+		}));
+	}
+
+	if (errorReason !== null) {
+		res.redirect('/#' +
+			querystring.stringify({
+				error: errorReason
+		}));
+	}
+	
+	var authOptions = {
+		url: 'https://connect.deezer.com/oauth/access_token.php?' +
+			querystring.stringify({
+				app_id: apiSecrets.deezerAppId,
+				secret: apiSecrets.deezerSecretKey,
+				code: code,
+			}),
+		json: true
+	};
+	
+	request.get(authOptions, function accessTokenResponse(error, response, body) {
+	
+		if (isBadResponse(error, response.statusCode)) {
+			res.redirect('/#' +
+				querystring.stringify({
+					error: 'invalid_token'
+			}));
+		}
+
+		var parsedResponse = querystring.parse(body);
+		var access_token = parsedResponse.access_token;
+
+		var options = {
+			url: 'http://api.deezer.com/user/me?' + querystring.stringify({ access_token: access_token }),
+			json: true
+		};
+		
+        request.get(options, function getDeezerUserResponse(error, response, body) {
+						
+			if (isBadResponse(error, response.statusCode)) {
+				res.redirect('/#' +
+					querystring.stringify({
+						error: 'invalid_deezer_details'
+				}));
+			}
+			
+			res.redirect('/#' +
+				querystring.stringify({
+					deezer_access_token: access_token,
+					deezer_user_id: response.body.id
 			}));
         });
 	});
